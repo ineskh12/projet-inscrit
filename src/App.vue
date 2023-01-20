53070c7eae9a91a78c2ff64fb82e0ff12710beb2
ines khelifi <52375090+ineskh12@users.noreply.github.com>
2023-01-20 13:30:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  
+
+  <h1>List des Inscrit</h1>
+  <InscritPage :liste="personnes"> </InscritPage>
+</template>
+
+<script>
+import InscritPage from './components/InscritPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    InscritPage
+  }, 
+  data(){
+    return{
+       personnes:[
+        {prenom:"Ines",nom:"Khelifi",age:29}
+       ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+
+</style>
---
+++ src/components/InscritPage.vue
@@ -0,0 +1,36 @@
+<template>
+ 
+  <table border >
+  <tr>
+    <th>Prénom</th>
+    <th>Nom</th>
+    <th>Agz</th>
+  </tr>
+
+ <tr v-for="(personne,index) in liste" :key="index">
+   <td>{{ personne.prenom }}</td>
+   <td>{{ personne.nom }}</td>
+   <td>{{ personne.age }}</td>
+ </tr>
+</table>
+
+</template>
+
+<script>
+export default {
+  name: 'InscritPage',
+  props: ['liste']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+th {
+  background: lightgray;
+  font-size: 140%;
+}
+td{
+  width: 10rem;
+  text-align: center;
+}
+</style>
